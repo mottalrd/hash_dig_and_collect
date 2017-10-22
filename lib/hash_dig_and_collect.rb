62250fbcb8e2e355eac9977ee7dcdc629de7e49f
Alfredo Motta <amotta@housetrip.com>
2017-10-22 10:11:17 +0100

    no need to dup if I can address the first next key explicitely with the params; better to use case statement than early return; use flat_map instead of explicitly do each_with_object and concat

--- lib/hash_dig_and_collect.rb
+++ lib/hash_dig_and_collect.rb
@@ -1,19 +1,17 @@
 module HashDigAndCollect
-  def dig_and_collect *keys
-    keys = keys.dup
-
-    next_key = keys.shift
+  def dig_and_collect(next_key, *keys)
     return [] unless self.has_key? next_key
 
     next_val = self[next_key]
     return [next_val] if keys.empty?
 
-    return next_val.dig_and_collect(*keys) if next_val.is_a? Hash
-
-    return [] unless next_val.is_a? Array
-    next_val.each_with_object([]) do |v, result|
-      inner = v.dig_and_collect(*keys)
-      result.concat inner
+    case next_val
+    when Hash
+      next_val.dig_and_collect(*keys)
+    when Array
+      next_val.flat_map { |v| v.dig_and_collect(*keys) }
+    else
+      []
     end
   end
 end
